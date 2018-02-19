e979863dcf42d90f10817126bdea04c05fb39d16
Vladimir Pavlov <thepavlovcompany@gmail.com>
2018-02-19 23:02:45 +0100

    Update app.js

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {setCurrentUserID, addMessage, addHistory} from '../actions';
 import ChatInput from '../components/ChatInput';
 import ChatHistory from '../components/ChatHistory';
 
-function mapStateToProps(/* state */) {
+function mapStateToProps(state) {
   return {
-    // TODO: Add state to be mapped to props
+    history: state.app.get('messages').toJS(),
+    userID: state.app.get('userID'),
+    lastMessageTimestamp: state.app.get('lastMessageTimestamp'),
   };
 }
 
-function mapDispatchToProps(/* dispatch */) {
+function mapDispatchToProps(dispatch) {
   return {
-    // TODO: Add actions to be mapped to props
+    addMessage: (message) => dispatch(addMessage(message)),
+    setUserID: (userID) => dispatch(setCurrentUserID(userID)),
+    addHistory: (messages, timestamp) => dispatch(addHistory(messages, timestamp)),
   };
 }
 
 class App extends React.Component {
-  state = {
-    userID: Math.round(Math.random() * 1000000).toString(),
-    history: [],
+  static propTypes = {
+    history: React.PropTypes.array,
+    userID: React.PropTypes.number,
+    addMessage: React.PropTypes.func,
+    setUserID: React.PropTypes.func,
+    addHistory: React.PropTypes.func,
+    lastMessageTimestamp: React.PropTypes.string,
   };
 
   componentDidMount() {
+    const ID = Math.round(Math.random() * 1000000);
+    this.props.setUserID(ID);
     this.PubNub = PUBNUB.init({
       publish_key: 'pub-c-912f7a52-dcae-4f17-ac62-66bce4ef29e2',
       subscribe_key: 'sub-c-24dd7414-119d-11e8-92ea-7a3d09c63f1f',
@@ -29,20 +40,30 @@ class App extends React.Component {
     });
     this.PubNub.subscribe({
       channel: 'ReactChat',
-      message: (message) => this.setState({
-        history: this.state.history.concat(message),
-      }),
+      message: this.props.addMessage,
     });
   }
 
   render() {
-    const {sendMessage, state} = this;
+    const {props, sendMessage} = this;
     return (
       <div>
-        <ChatHistory history = {state.history} />
-        <ChatInput userID = {state.userID} sendMessage = {sendMessage} />
+        <ChatHistory history={props.history} />
+        <ChatInput userID={props.userID} sendMessage={sendMessage} />
       </div>
     );
+  }
+
+  fetchHistory = () => {
+    const {props} = this;
+    this.PubNub.history({
+      channel: 'ReactChat',
+      count: 15,
+      start: props.lastMessageTimestamp,
+      callback: (data) => {
+        props.addHistory(data[0], data[1]);
+      },
+    });
   }
 
   sendMessage = (message) => {
