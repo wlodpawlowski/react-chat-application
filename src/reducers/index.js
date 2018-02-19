c3cd741cb05273351eb7bf4d2464a0a147db4f3c
Vladimir Pavlov <thepavlovcompany@gmail.com>
2018-02-19 23:04:53 +0100

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
+import appReducer from './app';
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
   routing: routerReducer,
+  app: appReducer,
 });
 
 export default rootReducer;
